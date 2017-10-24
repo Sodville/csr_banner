faa290a9d5b4152b70a091b8dcadc261ba8f627e
Vegard Blindheim <v.v.blindheim@gmail.com>
2017-10-24 18:49:42 +0200

    Lower bar

--- GD_feed.js
+++ GD_feed.js
@@ -15,24 +15,24 @@ $(document).ready(function(){
     // select * from htmlstring where url='https://live.givedirectly.org' and xpath='//div[@class="card recipient-card"]'
     // var yql = "https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20htmlstring%20WHERE%20url%3D%22https%3A%2F%2Flive.givedirectly.org%22%20and%20xpath%3D%22%2F%2Fdiv%5B%40id%3D'recipient-cards'%5D%2Fdiv%22%20%7C%20truncate(count%3D3)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
 
-    var yql = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20htmlstring%20where%20url%3D'https%3A%2F%2Flive.givedirectly.org'%20and%20xpath%3D'%2F%2Fdiv%5Bcontains(%40id%2C%22recipient-cards%22)%5D%2Fchild%3A%3A*'&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
+    var yql = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20htmlstring%20where%20url%3D'https%3A%2F%2Flive.givedirectly.org'%20and%20xpath%3D'%2F%2Fdiv%5Bcontains(%40id%2C%22recipient-cards%22)%5D%2Fchild%3A%3Adiv%5Bposition() <= 3%5D'&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
 
 
     $.get(yql, function(result){
 
             var res = result.query.results.result;
-            console.log(res);
+            console.log("res: ", res);
             res = res.replace(/<span class="card-highlight">/g, "<a href='https://live.givedirectly.org' class='profile-link'>");
             res = res.replace(/<\/span>/g, "<\/a>");
 
-            console.log("res:" + res);
+            console.log("res:", res);
 
             var recipientCards = document.getElementById("recipient-cards");
             // recipientCards.innerHTML = res;
 
             res = decodeHtml(res);
 
-            console.log(res);
+            console.log("res: ", res);
 
             $('#recipient-cards').html(res);
 
@@ -54,13 +54,29 @@ $(document).ready(function(){
                     link2.setAttribute("target", "_blank");
                     link2.setAttribute("href", newlink);
 
+                    //Flytt time seen
+                    console.log("tess: ", $(this).find('.recipient-card-right > .card-timeago'));
+                    $(this).find('.recipient-card-right > .card-timeago').prependTo(this);
+                    
                 })
 
-              console.log( "second success" );
+                // Fjern profillinker
+                $('.card-link-container').remove();
+
+                // Gjør om tittel
+                $('.card-name').each(function () {
+                    var text = $(this).text();
+                    console.log(text);
+                    var dollars = text.match(/\$\d*/i)[0];
+                    var name = text.slice(0, text.search("received"));
+                    console.log(name, dollars);
+                })
+
+                console.log( "second success" );
             })
 
             .fail(function() {
-              console.log( "error" );
+                console.log( "error" );
             })
 
 
