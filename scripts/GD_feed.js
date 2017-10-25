66549e67c1b66651058d940666136860e2484b7b
Vegard Blindheim <v.v.blindheim@gmail.com>
2017-10-25 11:46:18 +0200

    Nedre banner mer eller mindre MVP

--- scripts/GD_feed.js
+++ scripts/GD_feed.js
@@ -12,11 +12,14 @@ function decodeHtml(html) {
 
 $(document).ready(function(){
 
+    var config = {
+        feedItemsToFetch: 3
+    };
+
     // select * from htmlstring where url='https://live.givedirectly.org' and xpath='//div[@class="card recipient-card"]'
     // var yql = "https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20htmlstring%20WHERE%20url%3D%22https%3A%2F%2Flive.givedirectly.org%22%20and%20xpath%3D%22%2F%2Fdiv%5B%40id%3D'recipient-cards'%5D%2Fdiv%22%20%7C%20truncate(count%3D3)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
 
     var yql = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20htmlstring%20where%20url%3D'https%3A%2F%2Flive.givedirectly.org'%20and%20xpath%3D'%2F%2Fdiv%5Bcontains(%40id%2C%22recipient-cards%22)%5D%2Fchild%3A%3Adiv%5Bposition() <= 3%5D'&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
-
 
     $.get(yql, function(result){
 
@@ -63,14 +66,37 @@ $(document).ready(function(){
                 // Fjern profillinker
                 $('.card-link-container').remove();
 
-                // Gjør om tittel
+                // Gjør om tittel og sett inn nytt element for penger / "enrolled"
                 $('.card-name').each(function () {
-                    var text = $(this).text();
+                    var text = $(this).text().replace(/\s\s+/g, ' ');
                     console.log(text);
-                    var dollars = text.match(/\$\d*/i)[0];
-                    var name = text.slice(0, text.search("received"));
+                    var dollars, sliceWord;
+                    try {
+                        dollars = text.match(/\$\d*/g)[0];
+                        sliceWord = "received";
+                    } catch (TypeError) {
+                        sliceWord = dollars = "enrolled";
+                    }
+                    var name = text.slice(0, text.search(sliceWord) - 1);
                     console.log(name, dollars);
-                })
+                    $(this).text(name);
+                    $(this).after("<div class='card-money'>" + dollars + "</div>");
+                });
+
+                // VELDIG midlertidig måte å forkorte teksten på
+                $('.survey-answer-small').each(function () {
+                    var text = $(this).text().replace(/\s\s+/g, ' ');
+                    console.log(text);
+                    var wordArray = text.split(' ');
+                    console.log(wordArray);
+                    var newText = '';
+                    while( newText.length < 50 ) {
+                        newText += ' ' + wordArray.shift();
+                    }
+                    newText += ' ...'
+                    console.log(newText);
+                    $(this).text(newText);
+                });
 
                 console.log( "second success" );
             })
